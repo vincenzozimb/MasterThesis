265c1165f67fddca179aa9634220806827a45847
Vincenzo <vzimbardo9@gmail.com>
2023-11-07 11:40:12 +0100

    specific heat works!

--- code/src/test.jl
+++ code/src/test.jl
@@ -1,20 +1,30 @@
-using ITensors
+using DSP
+using Plots
 
-println("\n\n")
+let 
 
-i = Index(2)
-j = Index(5)
-k = Index(2)
+    # Generate a noisy signal
+    sample_rate = 1000.0  # Sample rate in Hz
+    t = 0:1/sample_rate:1  # Time vector
+    x = sin.(2π * 100 * t) + 0.5 * randn(length(t))  # Noisy signal (sinusoid + noise)
 
-A = randomITensor(i, j, k)
-@info inds(A)
+    # Plot the original signal
+    pl = plot(t, x, label="Noisy Signal")
+    # plot!(t, filtered_signal, label="Filtered Signal")
+    xlabel!("Time (s)")
+    ylabel!("Amplitude")
+    # title!("Low-Pass Filtered Signal")
+    
+    # filter
+    f_cutoff = 0.2
+    responsetype = Lowpass(10; fs=sample_rate)
+    designmethod = Butterworth(4)
+    xf = filt(digitalfilter(responsetype, designmethod), x)  
 
-U, S, V = svd(A, (i, k))
-F, T = factorize(A, (i,k))
+    # Plot the filtered signal
+    plot!(t, xf, label="Filtered Signal")
+    title!("Low-Pass Filtered Signal")
+    display(pl)
 
-
-
-@info inds(U)
-@info inds(F)
-# @info inds(S)
-# @info inds(V)
+    
+end
--- code/src/trg/trg.jl
+++ code/src/trg/trg.jl
@@ -1,115 +1,148 @@
 using ITensors
 using QuadGK
 using Plots
+using Statistics
 
-# function that calculates the partition function using TRG
-function trg(T::Float64, maxdim::Int, topscale::Int)
-    
-    dim0 = 2
-    s = Index(dim0,"scale=0")
-    
-    # indeces
-    l = addtags(s,"left");
-    r = addtags(s,"right");
-    u = addtags(s,"up");
-    d = addtags(s,"down");
+let
 
-    # lambda function that converts array integers into Ising spin integers 
-    Sig = (x) -> 1-2*(x-1)
+    ## function that calculates the partition function using TRG
+    function trg(K::Float64, maxdim::Int, topscale::Int)
         
-    # define and initialize tensor
-    A = ITensor(l,r,u,d)
-    
-    for sl in 1:dim0
-        for sd in 1:dim0
-            for sr in 1:dim0
-                for su in 1:dim0
-                    E = Sig(sl)*Sig(sd) + Sig(sd)*Sig(sr) + Sig(sr)*Sig(su) + Sig(su)*Sig(sl)
-                    P = exp(-E / T)
-                    A[l => sl, d => sd, r => sr, u => su] = P
+        dim0 = 2
+        s = Index(dim0,"scale=0")
+        
+        # indeces
+        l = addtags(s,"left");
+        d = addtags(s,"down");
+        r = addtags(s,"right");
+        u = addtags(s,"up");
+
+        # lambda function that converts array integers into Ising spin integers 
+        Sig = (x) -> 1-2*(x-1)
+            
+        # define and initialize tensor
+        A = ITensor(l,d,r,u)
+        
+        for sl in 1:dim0
+            for sd in 1:dim0
+                for sr in 1:dim0
+                    for su in 1:dim0
+                        E = Sig(sl)*Sig(sd) + Sig(sd)*Sig(sr) + Sig(sr)*Sig(su) + Sig(su)*Sig(sl)
+                        P = exp(K*E)
+                        A[l => sl, d => sd, r => sr, u => su] = P
+                    end
                 end
             end
         end
-    end
 
-    # TRG algorithm loop
-    Z = 1.0
-    for scale in 1:topscale
-        println("\n---------- Scale $scale -> $(1 + scale)  ----------")
-        Fl, Fr = factorize(A, (r,d); maxdim=maxdim, tags="left,scale=$scale")
-        Fu, Fd = factorize(A, (l,d); maxdim=maxdim, tags="up,scale=$scale")
+        # TRG algorithm loop
+        Z = 1.0
+        for scale in 1:topscale + 1
+            # println("\n---------- Scale $scale -> $(1 + scale)  ----------")
+            Fl, Fr = factorize(A, (d,r); maxdim=maxdim, tags="left,scale=$scale")
+            Fu, Fd = factorize(A, (l,d); maxdim=maxdim, tags="up,scale=$scale")
+            
+            l_new = commoninds(Fl,Fr)
+            r_new = replacetags(l_new, "left", "right")
+            Fr *= delta(l_new, r_new)
+
+            u_new = commoninds(Fu,Fd)
+            d_new = replacetags(u_new, "up", "down")
+            Fd *= delta(u_new, d_new)
+
+            Fl *= delta(r,l)
+            Fu *= delta(d,u)
+            Fr *= delta(l,r)
+            Fd *= delta(u,d)
+            
+            A = Fl * Fu * Fr * Fd
+
+            l = l_new
+            d = d_new
+            r = r_new
+            u = u_new
+            
+            trace = A * delta(l,r) * delta(u,d)
+            trace = scalar(trace)
+            A /= trace
+            Z *= trace ^ (1.0 / (2.0^(1+scale)))
+            # @info Z
+        end
+
+        return Z
         
-        l_new = commoninds(Fl,Fr)
-        r_new = replacetags(l_new, "left", "right")
-        Fr *= delta(l_new, r_new)
-
-        u_new = commoninds(Fu,Fd)
-        d_new = replacetags(u_new, "up", "down")
-        Fd *= delta(u_new, d_new)
-
-        Fl *= delta(r,l)
-        Fu *= delta(d,u)
-        Fr *= delta(l,r)
-        Fd *= delta(u,d)
-
-        A = Fl * Fu * Fr * Fd
-
-        l = l_new
-        r = r_new
-        u = u_new
-        d = d_new
-
-        trace = A*delta(l,r)*delta(u,d)
-        A /= trace
-        Z *= trace
-        # @info Z
     end
 
-    return scalar(Z)
+    
+    ## Ising exact free energy 
+    function ising_free_energy(β::Real, J::Real=1.0)
+        k = β * J
+        c = cosh(2 * k)
+        s = sinh(2 * k)
+        xmin = 0.0
+        xmax = π
+        integrand(x) = log(c^2 + √(s^4 + 1 - 2 * s^2 * cos(x)))
+        integral, err = quadgk(integrand, xmin, xmax)::Tuple{Float64,Float64}
+        return -(log(2) + integral / π) / (2 * β)
+    end
+
+
+    ## test 
+    
+    maxdim = 6
+    topscale = 12
+    
+    Tc = 2.2691853
+    
+    J = 1.0
+    ts = 0.1:0.2:8.1 # this is perfect
+    ks = J ./ ts
+    
+    @show "=====TRG======"
+    pf = []
+    cnt = size(ks,1)
+    for k in ks
+        y = trg(k, maxdim, topscale)
+        push!(pf,y)
+        print("\r count=$cnt")
+        flush(stdout)
+        cnt -= 1
+    end
+
+    F = - ts .* log.(pf)
+
+    pl1 = plot(ts, F, ls=:dot, lw=:3, label="free energy")
+    vline!([Tc], line=:red, label="T_c")   
+    Fexact = ising_free_energy.(J ./ ts)
+    plot!(ts, Fexact, label="free energy exact")
+    display(pl1)
+    
+    ## relative error in free energy
+    re = abs.(F - Fexact) ./ abs.(Fexact)
+    
+    pl2 = plot(ts, re, label="relative error")
+    vline!([Tc], line=:red, label="T_c")
+    display(pl2)
+
+
+    ## heat capacity
+    function heat_capacity(F, T)
+
+        delta_T = diff(T)
+        dF_dT = diff(F) ./ delta_T
+        d2F_dT2 = diff(dF_dT) ./ delta_T[1:end-1]    
+        # Calculate the heat capacity C = -T * d2F/dT2
+        C = -T[1:end-2] .* d2F_dT2
+        
+        return C
+    end
+
+    C = heat_capacity(F,ts)
+    Cexact = heat_capacity(Fexact,ts)
+    vline!([Tc], line=:red, label="T_c")
+    pl3 = plot(ts[1:end-2], C, ls=:dot, lw=:3, label="specific heat")
+    plot!(ts[1:end-2], Cexact, label="heat capacity exact")
+    display(pl3)
+
 
 end
-
-
-# test 
-
-T = 2.0
-maxdim = 12
-topscale = 10
-
-# Z = trg(T, maxdim, topscale)
-# println(Z)
-
-
-function ising_free_energy(β::Real, J::Real=1.0)
-    k = β * J
-    c = cosh(2 * k)
-    s = sinh(2 * k)
-    xmin = 0.0
-    xmax = π
-    integrand(x) = log(c^2 + √(s^4 + 1 - 2 * s^2 * cos(x)))
-    integral, err = quadgk(integrand, xmin, xmax)::Tuple{Float64,Float64}
-    return -(log(2) + integral / π) / (2 * β)
-end
-  
-function ising_magnetization(β::Real)
-    β > βc && return (1 - sinh(2 * β)^(-4))^(1 / 8)
-    return 0.0
-end
-
-
-ts = 0.1:0.01:3.0
-@show "=====TRG======"
-Z = []
-for K in ts
-    y = trg(K, maxdim, topscale)
-    push!(Z,y)
-end
-F = - ts .* log.(Z)
-beta = 1 ./ ts
-
-pl1 = plot(ts, F)
-
-Fexact = ising_free_energy.(1.0 ./ ts)
-pl2 = plot!(ts, Fexact)
-display(pl1)
-
