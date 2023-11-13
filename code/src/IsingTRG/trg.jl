1d4716fb4a8498bbbe7d1e111041bfb9398f3f84
Vincenzo <vzimbardo9@gmail.com>
2023-11-13 19:44:37 +0100

    modifications not finished

--- code/src/IsingTRG/trg.jl
+++ code/src/IsingTRG/trg.jl
@@ -1,9 +1,35 @@
 using ITensors
+using QuadGK
+using ForwardDiff
+
+## Square lattice Ising exact free energy 
+function ising_free_energy(β::Real, J::Real=1.0)
+    k = β * J
+    c = cosh(2 * k)
+    s = sinh(2 * k)
+    xmin = 0.0
+    xmax = π
+    integrand(x) = log(c^2 + √(s^4 + 1 - 2 * s^2 * cos(2.0*x)))
+    integral, err = quadgk(integrand, xmin, xmax)::Tuple{Float64,Float64}
+    return -(log(2) + integral / π) / (2 * β)
+end
 
 
-## function that calculates the partition function using TRG
-function trg(K::Float64, maxdim::Int, topscale::Int, h::Float64)
-        
+## Square lattice Ising exact magnetization
+function ising_magnetization(β::Real)
+    β > βc && return (1 - sinh(2 * β)^(-4))^(1 / 8)
+    return 0.0
+end
+
+# dlnZ(K::Vector) = ForwardDiff.gradient(K->lnZ(K[1]), K)[1];
+# dlnZ2(K::Vector) = ForwardDiff.gradient(K->dlnZ(K), K)[1];
+
+
+## function that calculates the elementary tensor in the chessboard representation
+function tensor_chess(J::Real, h::Real)
+    
+    # The coupling constants are actually the adimensional ones. That is J is actually βJ and h is actually βh
+
     dim0 = 2
     s = Index(dim0,"scale=0")
     
@@ -13,7 +39,7 @@ function trg(K::Float64, maxdim::Int, topscale::Int, h::Float64)
     r = addtags(s,"right");
     u = addtags(s,"up");
 
-    # lambda function that converts array integers into Ising spin integers 
+    # lambda function that converts array integers into Ising spin integers (1,2) -> (1,-1)
     Sig = (x) -> 1-2*(x-1)
         
     # define and initialize tensor
@@ -23,14 +49,30 @@ function trg(K::Float64, maxdim::Int, topscale::Int, h::Float64)
         for sd in 1:dim0
             for sr in 1:dim0
                 for su in 1:dim0
-                    E = Sig(sl)*Sig(sd) + Sig(sd)*Sig(sr) + Sig(sr)*Sig(su) + Sig(su)*Sig(sl)
+                    Eint = Sig(sl)*Sig(sd) + Sig(sd)*Sig(sr) + Sig(sr)*Sig(su) + Sig(su)*Sig(sl)
                     Emag = (Sig(sl) + Sig(sd) + Sig(sr) + Sig(su)) / 2.0
-                    P = exp(K*E + h*Emag)
+                    P = exp(J*Eint + h*Emag)
                     A[l => sl, d => sd, r => sr, u => su] = P
                 end
             end
         end
     end
+
+    return A
+
+end
+
+
+## function that calculates the log-partition function using TRG
+function trg(A::ITensor, maxdim::Int, topscale::Int)
+        
+    # check A tensor. It is configured to Ising models. It can be modified
+    @assert size(A) == (2,2,2,2)
+    @assert length(inds(A)) == 4
+    @assert hastags(tags(inds(A)[1]), "left,scale=0")
+    @assert hastags(tags(inds(A)[2]), "down,scale=0")
+    @assert hastags(tags(inds(A)[3]), "right,scale=0")
+    @assert hastags(tags(inds(A)[4]), "up,scale=0")
 
     # TRG algorithm loop
     Z = 1.0
@@ -65,6 +107,9 @@ function trg(K::Float64, maxdim::Int, topscale::Int, h::Float64)
         Z *= trace ^ (1.0 / (2.0^(1+scale)))
     end
 
-    return Z
+    return log(Z)
     
 end
+
+
+
--- code/test/test.jl
+++ code/test/test.jl
@@ -1,20 +1,100 @@
 # using DSP
-using Plots
+# using Plots
+# using ITensors
 
-let 
+# include("../src/IsingTRG/trg.jl") 
 
-    function f(x, y)
-        x[1] = 42    # mutates x
-        y += 7    # new binding for y, no mutation
-        return y
+using LinearAlgebra
+using TensorOperations
+
+function TRG(K::Float64,iteration::Int64,maxD::Int64)
+
+    T = zeros(Float64,2,2,2,2)
+
+    no2s(no::Int64) = -2*no+3
+    sumS(s0::Int64,s1::Int64,s2::Int64,s3::Int64) = no2s(s0)*no2s(s1)+no2s(s1)*no2s(s2)+no2s(s2)*no2s(s3)+no2s(s3)*no2s(s0)
+
+    for s0 = 1:2,s1 = 1:2,s2 = 1:2,s3 = 1:2
+        @inbounds T[s3,s2,s1,s0] = exp(K*sumS(s3,s2,s1,s0))
     end
 
-    a = [1,2,3]
-    b = 2
+    D = 2
+    epsilon = 0.0
 
-    f(a,b)
+    checkBigger(M::Float64) = M>=epsilon
 
-    b
+    lnZ = 0.0
+
+    @inbounds for n = 1:iteration
+        maxVal = maximum(T)
+        T /= maxVal
+        lnZ += 2^(iteration-n+1)*log(maxVal)
+        TypeA = reshape(T,(D^2,D^2))
+        TypeB = reshape(permutedims(T,[1 4 2 3]),(D^2,D^2))
+        ua,sa,va = svd(TypeA)
+        ub,sb,vb = svd(TypeB)
+        cut = min(count(checkBigger,sb),count(checkBigger,sa),maxD)
+        S2 = reshape(view(ub,:,1:cut)*Diagonal(sqrt.(view(sb,1:cut))),(D,D,cut))
+        S1 = reshape(Diagonal(sqrt.(view(sb,1:cut)))*view(transpose(vb),1:cut,:),(cut,D,D))
+        S3 = reshape(view(ua,:,1:cut)*Diagonal(sqrt.(view(sa,1:cut))),(D,D,cut))
+        S4 = reshape(Diagonal(sqrt.(view(sa,1:cut)))*view(transpose(va),1:cut,:),(cut,D,D))
+
+        @tensor T[a,e,g,n] := S1[a,b,c]*S3[c,d,e]*S2[f,d,g]*S4[n,f,b]
+        #T = Tp
+        D = cut
+    end
+
+    trace = 0.0
+    for i = 1:D
+        @inbounds trace += T[i,i,i,i]
+    end
+    lnZ += log(trace)
+
+    println("K:",K)
+    println("lnZ/N:",lnZ/(2^(iteration+1)))
+end
+
+function main(args)
+    if isempty(args)
+        K = 1.0
+        iters = 20
+        cut = 20
+    else
+        K = parse(Float64,args[1])
+        iters = parse(Int64,args[2])
+        cut = parse(Int64,args[3])
+    end
+    TRG(K,iters,cut)
+end
+
+main(ARGS)
+
+
+
+# let 
+    
+#     beta = 1.0
+#     J = 1.0 * beta
+#     h = 0.1 * beta
+
+#     A = tensor_chess(J, h)
+    
+#     @assert size(A) == (2,2,2,2)
+#     @assert length(inds(A)) == 4
+#     @assert hastags(tags(inds(A)[1]), "left,scale=0")
+#     @assert hastags(tags(inds(A)[2]), "down,scale=0")
+#     @assert hastags(tags(inds(A)[3]), "right,scale=0")
+#     @assert hastags(tags(inds(A)[4]), "up,scale=0")
+
+
+    
+    
+
+    
+# end
+
+
+
 
     # # Generate a noisy signal
     # sample_rate = 1000.0  # Sample rate in Hz
@@ -40,4 +120,3 @@ let
     # display(pl)
 
     
-end
